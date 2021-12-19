d8dc527ee8902b83e7fa7cdcfb06d38dcd85a511
Guanzhao Wang <g95wang@gmail.com>
2021-12-19 18:12:47 -0500

    basic kmeans

--- mylibs/clustering.py
+++ mylibs/clustering.py
@@ -1,2 +1,28 @@
-def label_data(unlabeled_data):
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+def label_data(unlabeled_data, num_classes):
+    print("Labeling unlabeled data...")
+    offset = 10 - num_classes
+    images = np.array([x[0].numpy() for x in unlabeled_data])
+    actual_labels = np.array([x[1] for x in unlabeled_data])
+    # pca_2 = PCA(n_components=2)
+    # pca_2_result = pca_2.fit_transform(images)
+    # print(pca_2_result.shape)
+
+    # kmeans clustering the images
+    kmeans = KMeans(n_clusters=num_classes).fit(images.reshape(images.shape[0], -1))
+    predict_labels = kmeans.labels_ + offset
+
+    # relabel to match the original labels to be consistent with test dataset
+    rearrange_labels = np.copy(predict_labels)
+    cmatrix = confusion_matrix(predict_labels, actual_labels)
+    for i in range(num_classes):
+        mask = predict_labels == i + offset
+        rearrange_labels[mask] = np.argmax(cmatrix[i]) + offset
+        
+    accuracy = np.sum(rearrange_labels == actual_labels) / len(actual_labels)
+    print("Labeling accuracy: {}".format(accuracy))
     return unlabeled_data
--- mylibs/dataloader.py
+++ mylibs/dataloader.py
@@ -8,13 +8,20 @@ import random
 from mylibs.clustering import label_data
 
 class CustomFashionMNIST(Dataset):
-    def __init__(self, train, include_labels, transform, use_unlabeled):
+    # mode:
+    # 0 - use only labeled data
+    # 1 - use labeled data and clustering-labeled unlabeled data
+    # 2 - use full FasionMNIST data
+    def __init__(self, train, include_labels, transform, mode=0):
         super(Dataset, self).__init__()
         raw_data = torchvision.datasets.FashionMNIST(root='../data/', download=True, train=train, transform=transform)
         self.labeled_data = [(img, label) for img, label in raw_data if label in include_labels]
         self.unlabeled_data = [(img, label) for img, label in raw_data if label not in include_labels]
-        if use_unlabeled:
-            self.unlabeled_data = label_data(self.unlabeled_data)
+        if mode == 0:
+            self.labeled_data.extend(self.unlabeled_data)
+            self.data = self.labeled_data
+        elif mode == 1:
+            self.unlabeled_data = label_data(self.unlabeled_data, num_classes=10-len(include_labels))
             self.labeled_data.extend(self.unlabeled_data)
             self.data = self.labeled_data
         else:
@@ -42,10 +49,10 @@ class CustomFashionMNIST(Dataset):
             plt.axis('off')
         plt.show()
 
-def getDataLoaders(include_labels=range(9), transform=None, train_batch_size=64, test_batch_size=1, num_workers=1, use_unlabeled=False):
+def getDataLoaders(include_labels=range(9), transform=None, train_batch_size=64, test_batch_size=1, num_workers=1, mode=2):
     # add validation set?
-    train_set = CustomFashionMNIST(train=True, include_labels=include_labels, transform=transform, use_unlabeled=use_unlabeled)
-    test_set = CustomFashionMNIST(train=False, include_labels=include_labels, transform=transform, use_unlabeled=use_unlabeled)
+    train_set = CustomFashionMNIST(train=True, include_labels=include_labels, transform=transform, mode=mode)
+    test_set = CustomFashionMNIST(train=False, include_labels=include_labels, transform=transform, mode=2)
     train_dataloader = DataLoader(train_set, batch_size=train_batch_size, shuffle=True, num_workers=num_workers)
     test_dataloader = DataLoader(test_set, batch_size=test_batch_size, shuffle=True, num_workers=num_workers)
     return train_dataloader, test_dataloader
