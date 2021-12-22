8fc6a89b8a22031e73509756d95774b6d4af3b4b
Your Name <kewa990809@gmail.com>
2021-12-21 20:28:30 -0500

    transform

--- mylibs/transform.py
+++ mylibs/transform.py
@@ -2,7 +2,19 @@ import torchvision
 from torchvision import transforms
 
 data = torchvision.datasets.FashionMNIST(download=False, train=True, root="../data").train_data.float()
-transform = transforms.Compose([
+
+transform_n = transforms.Compose([
     transforms.ToTensor(),
+    transforms.Normalize((data.mean()/255,), (data.std()/255,))
+])
+
+transform_t = transforms.Compose([
+    transforms.ToTensor()
+])
+
+transform_aug = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.RandomHorizontalFlip(0.5),
+    transforms.RandomCrop(28),
     transforms.Normalize((data.mean()/255,), (data.std()/255,))
 ])
